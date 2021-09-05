682570eba36267c58acd1df797fd65d4b511ebee
mnn <>
2021-09-05 17:01:41 +0530

    Edited program so that it checks for the first prime number greater than or equal to the inputted number.

--- checkprime.py
+++ checkprime.py
@@ -1,15 +1,18 @@
 ###############################################################
-# Checks if a numebr is prime or composite.
+# Checks if a number is prime or composite.
+# If composite, scans for a prime number greater than it.
 ###############################################################
 
-#CONSTANTS
 
+
+from math import sqrt
+import time
 
 def checkprime(orig):
     if orig == 0 or orig == 1:
         return 'Neither Prime nor composite'
     
-    h = int(orig/2)
+    h = int(sqrt(orig))
     for i in range(2, h):
         if orig % i == 0:
             return f'Composite: {i} x {int(orig/i)} = {orig}'
@@ -17,19 +20,26 @@ def checkprime(orig):
             
     return 'Prime'
         
-
-#def test():
-#   for num in range(100):
-#      res = checkprime(num)
-#     print(num, 'is', res)
-
-
+def greaterprime(num):
+    if num % 2 == 0:
+        inc = 1
+    else:
+        inc = 0
+    while True:
+        chk = num + inc
+        is_prime = checkprime(chk)
+        if is_prime == 'Prime':
+            return chk
+        else:
+            inc += 2            
 
 
 ########################## MAIN PROGRAM ##########################
 while True:
-    num = int(input("Enter a number: "))
-    res = checkprime(num)
+    num = int(input("Enter a number to begin scanning primes greater than that: "))
+    start = time.time()
+    res = greaterprime(num)
+    end = time.time()
+    print(f'Yay! Found prime number in {end - start} secs!')
     print(res)
-
         
